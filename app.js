e9caddcb0788064461e3b09e5a4939f24a69c9ea
angelagk <79419235+angelagk@users.noreply.github.com>
2021-03-28 15:42:23 -0500

    pjs

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
+const INITIAL_COLOR = "#2c2c2c";
+
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = "INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
 ctx.lineWidth = 2.5;
+
 
 let painting = false;
 let filling = false;
@@ -38,6 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -52,7 +57,15 @@ function handleModeClick(){
     }else{
         filling = true;
         mode.innerText = "Paint";
+        
     }
+}
+
+function handleCanvasClick(){
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);  
+    }
+    
 }
 
 
@@ -61,6 +74,7 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
